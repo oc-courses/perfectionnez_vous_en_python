53868de575611098e493d80c7438a50519050db0
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-05-09 17:46:35 +0200

    P2C1

--- mymodule.py
+++ mymodule.py
@@ -1,22 +1,63 @@
-def launch_analysis(data_file):
-    
-    # If data_file is an absolute path, then the folder "data" is ignored
-    # and path_to_file will equals data_file
-    # If not, "data" will be append at the beginning of the path
-    path_to_file = os.path.join("data",data_file)
-    
-    file_name = os.path.basename(path_to_file)
-    directory = os.path.dirname(path_to_file)
-    print("Opening data file {} from directory '{}'".format(file_name,directory))
-    
-    try:
-        with open(path_to_file,"r") as f:
-            preview = f.readline()
-        print("Yeah! We managed to read the file. Here is a preview:")
-        print(preview)
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+# The following line is a bit magical: it makes matplolib graph
+# more beautiful by modifying the default style
+import seaborn as sns
+
+class SetOfParliamentMember:
+    def __init__(self, name):
+        self.name = name
         
-    except FileNotFoundError as e:
-        print("Ow :( The file was not found. Here is the original message of the exception :")
-        # e is a FileNotFoundError, which is also an Exception (by inheritance).
-        # print(e) prints the error message (which is a string) of this Exception.
-        print(e)
+    def data_from_csv(self, csv_file):
+        self.dataframe = pd.read_csv(csv_file, sep=";")
+        
+    def data_from_dataframe(self, dataframe):
+        self.dataframe = dataframe
+        
+    def display_chart(self):
+        data = self.dataframe
+        female_mps = data[data.sexe == "F"]
+        male_mps = data[data.sexe == "H"]
+        
+        counts = [len(female_mps), len(male_mps)]
+        counts = np.array(counts)
+        nb_mps = counts.sum()
+        proportions = counts / nb_mps
+        
+        labels = ["Female ({})".format(counts[0]), "Male ({})".format(counts[1])]
+        
+        fig, ax = plt.subplots()
+        ax.axis("equal")
+        ax.pie(
+                proportions,
+                labels=labels,
+                autopct="%1.1f%%"
+                )
+        plt.title("{} ({} MPs)".format(self.name, nb_mps))
+        plt.show()
+        
+    def split_by_political_party(self):
+        result = {}
+        data = self.dataframe
+        
+        # These 2 syntaxes are equivalent : data.parti_ratt_financier and data['parti_ratt_financier']
+        all_parties = data["parti_ratt_financier"].dropna().unique()
+            
+        for party in all_parties:
+            data_subset = data[data.parti_ratt_financier == party]
+            subset = SetOfParliamentMember('MPs from party "{}"'.format(party))
+            subset.data_from_dataframe(data_subset)
+            result[party] = subset
+        
+        return result
+
+
+# def launch_analysis(data_file, by_party = False):
+#     sopm = SetOfParliamentMember("All MPs")
+#     sopm.data_from_csv(os.path.join("data",data_file))
+#     sopm.display_chart()
+    
+#     if by_party:
+#         for party, s in sopm.split_by_political_party().items():
+#             s.display_chart()
--- parite.py
+++ parite.py
@@ -1,15 +1,12 @@
-#/bin/bash
-import os
-import argparse
-if not NOTEBOOK:
-    import mymodule
-    
 def parse_arguments():
     
     if not NOTEBOOK:
         parser = argparse.ArgumentParser()
+        
         parser.add_argument("-d","--datafile",help="""CSV file containing pieces of 
             information about the members of parliament""")
+        parser.add_argument("-p","--byparty",action='store_true',help="displays a graph for each political party")
+        
         return parser.parse_args()
     
     else: ## todo: A enlever. Ici on triche car ArgumentParser est innutilisable dans un notebook
@@ -17,10 +14,15 @@ def parse_arguments():
             pass
         o = foo()
         o.__dict__ = {
-            'datafile' : "current_mps.csv"
+            'datafile' : "current_mps.csv",
+            'byparty' : True,
         }
         return o
     
 if __name__ == '__main__':
     args = parse_arguments()    
-    launch_analysis(args.datafile)
+    launch_analysis(args.datafile, args.byparty)
+
+    
+if NOTEBOOK:
+    Sauv = SetOfParliamentMember # sauvegarde de la classe
