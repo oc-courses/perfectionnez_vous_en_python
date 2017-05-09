c4f6de7e169dbd6602ca1bef1bfbae9a288a5831
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-05-09 17:42:43 +0200

    P1C3

--- parite.py
+++ parite.py
@@ -1,1 +1,26 @@
 #/bin/bash
+import os
+import argparse
+if not NOTEBOOK:
+    import mymodule
+    
+def parse_arguments():
+    
+    if not NOTEBOOK:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-d","--datafile",help="""CSV file containing pieces of 
+            information about the members of parliament""")
+        return parser.parse_args()
+    
+    else: ## todo: A enlever. Ici on triche car ArgumentParser est innutilisable dans un notebook
+        class foo:
+            pass
+        o = foo()
+        o.__dict__ = {
+            'datafile' : "current_mps.csv"
+        }
+        return o
+    
+if __name__ == '__main__':
+    args = parse_arguments()    
+    launch_analysis(args.datafile)
