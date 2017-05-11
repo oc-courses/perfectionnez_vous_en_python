0d87ba9b5c2f7a14a9c7d9d18418ab471a2d4644
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-05-11 13:23:23 +0200

    a new file for a new analysis

---
+++ xml_analysis.py
@@ -0,0 +1,14 @@
+## todo: je ne sais pas si c'est très "classe", mais moi j'ai bien envie
+## de leur faire tester du code qui va potentiellemnt faire planter leur ordi,
+## pour qu'il se rendent compte du problème, problème que l'on va chercher à résoudre
+## ensuite
+
+from lxml import etree
+import os
+
+I_KNOW_WHAT_I_M_DOING = False
+
+# Caution : This code might need a lot of time and ressources for your computer
+# Save any unsaved work
+if I_KNOW_WHAT_I_M_DOING:
+    etree.XML(os.path.join("data","SyceronBrut.xml"))
