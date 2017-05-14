345d612233d1ef2a9b38db3a0f83f34af0748a6e
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-05-14 18:26:47 +0200

    change many thinks, including file structure and arguments

--- parite.py
+++ parite.py
@@ -1,14 +1,19 @@
-#/bin/bash
-import os
+#! /usr/bin/env python3
+# coding: utf-8
 import argparse
-import analysis as an
-    
+import analysis.csv as c_an
+import analysis.xml as x_an
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d","--datafile",help="""CSV file containing pieces of 
+    parser.add_argument("-d","--datafile",help="""CSV file containing pieces of
         information about the members of parliament""")
+    parser.add_argument("-e", "--extension", help="""King of of file to analyse. Is it a CSV of an XML?""")
     return parser.parse_args()
-    
+
 if __name__ == '__main__':
-    args = parse_arguments()    
-    an.launch_analysis(args.datafile)
+    args = parse_arguments()
+    if args.extension == 'xml':
+        x_an.launch_analysis(args.datafile)
+    elif args.extension == 'csv':
+        c_an.launch_analysis(args.datafile)
