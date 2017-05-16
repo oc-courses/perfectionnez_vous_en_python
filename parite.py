db2b34f9aac4e852f354e6a194a1273351b61c60
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-05-16 19:36:02 +0200

    P2C4

--- parite.py
+++ parite.py
@@ -11,7 +11,6 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d","--datafile",help="""CSV file containing pieces of
         information about the members of parliament""")
-    parser.add_argument("-e", "--extension", help="""King of of file to analyse. Is it a CSV of an XML?""")
     parser.add_argument("-p","--byparty", action='store_true', help="""displays
         a graph for each political party""")
     parser.add_argument("-i","--info", action='store_true', help="""information about
@@ -22,7 +21,7 @@ def parse_arguments():
     parser.add_argument("-I","--index", help="""displays information about the Ith mp""")
     parser.add_argument("-g","--groupfirst", help="""displays a graph groupping all the 'g'
         biggest political parties""")
-    #parser.add_argument("-a","--byage", help="""displays a graph for the MPs splitted
+    parser.add_argument("-a","--byage", help="""displays a graph for the MPs splitted
     #        between those who are over and those who are under the value of --byage""")
     return parser.parse_args()
 
@@ -35,9 +34,12 @@ if __name__ == '__main__':
     except Warning as e:
         lg.warning(e)
     else:
-        if args.extension == 'xml':
+        #import pdb; pdb.set_trace()
+        e = re.search('^.+\.(\D{3})$', args.datafile)
+        extension = e.group(1)
+        if extension == 'xml':
             x_an.launch_analysis(datafile)
-        elif args.extension == 'csv':
+        elif extension == 'csv':
             c_an.launch_analysis(datafile, args.byparty, args.info, args.displaynames,
                            args.searchname, args.index, args.groupfirst, args.byage)
     finally:
