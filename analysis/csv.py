253739dc9575f690f45f114e63f6e1b52407bd98
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-05-18 16:19:55 +0200

    P2C2

--- analysis/csv.py
+++ analysis/csv.py
@@ -1,3 +1,7 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+
 import os
 import pprint
 import logging as lg
--- parite.py
+++ parite.py
@@ -1,3 +1,11 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""
+============================================
+  Analyse Woman / Man balance in Politics
+============================================
+"""
 import argparse
 # import pdb
 import logging as lg
@@ -8,35 +16,39 @@ import analysis.xml as x_an
 lg.basicConfig(level=lg.DEBUG)
 
 def parse_arguments():
+    """ Get arguments from the command line
+    to get different results according to our needs.
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d","--datafile",help="""CSV file containing pieces of
+    parser.add_argument("-d", "--datafile", help="""CSV file containing pieces of
         information about the members of parliament""")
-    parser.add_argument("-e", "--extension", help="""King of of file to analyse. Is it a CSV of an XML?""")
-    parser.add_argument("-p","--byparty", action='store_true', help="""displays
+    parser.add_argument("-e", "--extension", help="""King of of file to analyse. \
+     Is it a CSV of an XML?""")
+    parser.add_argument("-p", "--byparty", action='store_true', help="""displays
         a graph for each political party""")
-    parser.add_argument("-i","--info", action='store_true', help="""information about
+    parser.add_argument("-i", "--info", action='store_true', help="""information about
         the file""")
-    parser.add_argument("-n","--displaynames", action='store_true', help="""displays
+    parser.add_argument("-n", "--displaynames", action='store_true', help="""displays
         the names of all the mps""")
-    parser.add_argument("-s","--searchname", help="""search for a mp name""")
-    parser.add_argument("-I","--index", help="""displays information about the Ith mp""")
-    parser.add_argument("-g","--groupfirst", help="""displays a graph groupping all the 'g'
+    parser.add_argument("-s", "--searchname", help="""search for a mp name""")
+    parser.add_argument("-I", "--index", help="""displays information about the Ith mp""")
+    parser.add_argument("-g", "--groupfirst", help="""displays a graph groupping all the 'g'
         biggest political parties""")
     return parser.parse_args()
 
 if __name__ == '__main__':
-    args = parse_arguments()
+    ARGS = parse_arguments()
     try:
-        datafile = args.datafile
-        if datafile == None:
+        DATAFILE = ARGS.datafile
+        if DATAFILE is None:
             raise Warning('You must indicate a datafile!')
-    except Warning as e:
-        lg.warning(e)
+    except Warning as exception:
+        lg.warning(exception)
     else:
-        if args.extension == 'xml':
-            x_an.launch_analysis(datafile)
-        elif args.extension == 'csv':
-            c_an.launch_analysis(datafile, args.byparty, args.info, args.displaynames,
-                           args.searchname, args.index, args.groupfirst)
+        if ARGS.extension == 'xml':
+            x_an.launch_analysis(DATAFILE)
+        elif ARGS.extension == 'csv':
+            c_an.launch_analysis(DATAFILE, ARGS.byparty, ARGS.info, ARGS.displaynames, \
+            ARGS.searchname, ARGS.index, ARGS.groupfirst)
     finally:
         lg.info('#################### Analysis is over ######################')
