04f409eb8ced99c5ac10816481dcbcf21053d0cb
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-05-18 17:44:24 +0200

    regis's comments

--- analysis/xml.py
+++ analysis/xml.py
@@ -4,18 +4,11 @@
 import os
 
 def launch_analysis(data_file):
-
-    # If data_file is an absolute path, then the folder "data" is ignored
-    # and path_to_file will equals data_file
-    # If not, "data" will be append at the beginning of the path
-    path_to_file = os.path.join("data", data_file)
-
-    file_name = os.path.basename(path_to_file)
-    directory = os.path.dirname(path_to_file)
-    print("Opening data file {} from directory '{}'".format(file_name,directory))
+    directory = os.path.dirname(__file__) # we get the right path.
+    path_to_file = os.path.join(directory, "data", data_file) # with this path, we go inside the folder `data` and get the file.
 
     with open(path_to_file,"r") as f:
-       preview = f.readline() # read first line
+        preview = f.readline()
 
     print("Yeah! We managed to read the file. Here is a preview: {}".format(preview))
 
