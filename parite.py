4e458e7c772a5e951cbf99cab469a735d6830fc3
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-05-18 21:41:08 +0200

    .

--- parite.py
+++ parite.py
@@ -7,7 +7,7 @@
 ============================================
 """
 import argparse
-# import pdb
+import re
 import logging as lg
 
 import analysis.csv as c_an
@@ -22,8 +22,6 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--datafile", help="""CSV file containing pieces of
         information about the members of parliament""")
-    parser.add_argument("-e", "--extension", help="""King of of file to analyse. \
-     Is it a CSV of an XML?""")
     parser.add_argument("-p", "--byparty", action='store_true', help="""displays
         a graph for each political party""")
     parser.add_argument("-i", "--info", action='store_true', help="""information about
@@ -32,6 +30,8 @@ def parse_arguments():
         the names of all the mps""")
     parser.add_argument("-s", "--searchname", help="""search for a mp name""")
     parser.add_argument("-I", "--index", help="""displays information about the Ith mp""")
+    parser.add_argument("-a","--byage", help="""displays a graph for the MPs splitted
+    #        between those who are over and those who are under the value of --byage""")
     parser.add_argument("-g", "--groupfirst", help="""displays a graph groupping all the 'g'
         biggest political parties""")
     return parser.parse_args()
@@ -45,10 +45,12 @@ if __name__ == '__main__':
     except Warning as exception:
         lg.warning(exception)
     else:
-        if ARGS.extension == 'xml':
+        e = re.search('^.+\.(\D{3})$', ARGS.datafile)
+        EXTENSION = e.group(1)
+        if EXTENSION == 'xml':
             x_an.launch_analysis(DATAFILE)
-        elif ARGS.extension == 'csv':
-            c_an.launch_analysis(DATAFILE, ARGS.byparty, ARGS.info, ARGS.displaynames, \
-            ARGS.searchname, ARGS.index, ARGS.groupfirst)
+        elif EXTENSION == 'csv':
+            c_an.launch_analysis(DATAFILE, ARGS.byparty, ARGS.info, ARGS.displaynames,
+                           ARGS.searchname, ARGS.index, ARGS.groupfirst, ARGS.byage)
     finally:
         lg.info('#################### Analysis is over ######################')
