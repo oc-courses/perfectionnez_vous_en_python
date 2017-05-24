1d18c1a92750f3323f142b19e8218dd15cf779a9
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2017-05-24 12:40:55 +0200

    Update parite.py

--- parite.py
+++ parite.py
@@ -1,19 +1,21 @@
 #! /usr/bin/env python3
 # coding: utf-8
-
 import argparse
+
 import analysis.csv as c_an
 import analysis.xml as x_an
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d","--datafile",help="""CSV file containing pieces of information about the members of parliament""")
-    parser.add_argument("-e", "--extension", help="""King of of file to analyse. Is it a CSV of an XML?""")
+    parser.add_argument("-e", "--extension", help="""Type of file to analyse. Is it a CSV of an XML?""")
     return parser.parse_args()
 
-if __name__ == '__main__':
+def main():
     args = parse_arguments()
     if args.extension == 'xml':
-        x_an.launch_analysis(args.datafile)
+        x_an.launch_analysis('SyceronBrut.xml')
     elif args.extension == 'csv':
-        c_an.launch_analysis(args.datafile)
+        c_an.launch_analysis('current_mps.csv')
+
+if __name__ == "__main__":
+    main()
