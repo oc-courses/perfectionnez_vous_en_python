5c63970fe57beb1b14d7414c2701e1b8244d853c
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2017-08-17 11:10:44 +0200

    Update parite.py

--- parite.py
+++ parite.py
@@ -9,7 +9,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d","--datafile",help="""CSV file containing pieces of
         information about the members of parliament""")
-    parser.add_argument("-e", "--extension", help="""King of of file to analyse. Is it a CSV of an XML?""")
+    parser.add_argument("-e", "--extension", help="""Kind of file to analyse. Is it a CSV or an XML?""")
     parser.add_argument("-v", "--verbose", action='store_true', help="""Make the application talk!""")
     return parser.parse_args()
 
